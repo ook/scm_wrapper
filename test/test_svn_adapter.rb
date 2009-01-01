23ecd719f7301c6fcd9056f6b48a48505b26614a
Thomas Lecavelier <thomas@lecavelier.name>
2009-01-01 14:59:11 +0100

    Prepare setup for testcases

--- test/test_svn_adapter.rb
+++ test/test_svn_adapter.rb
@@ -2,7 +2,15 @@ require 'test/unit'
 require 'lib/scm_wrapper/svn_adapter'
 
 class TestSvnAdapter < Test::Unit::TestCase
+  REDMINE_REPO = 'http://redmine.rubyforge.org/svn/trunk'
+
+  def setup
+    @s = ScmWrapper::SvnAdapter.new
+    @s.url = REDMINE_REPO
+  end
+
   def test_log
-    assert true
+    r = @s.log(2000, 2005)
+    assert_equal 6, r.size
   end
 end
